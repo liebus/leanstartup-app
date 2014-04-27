592f7885997fca437e017e288aceb380b54b756c
liebus <lima.hane@gmail.com>
2014-04-27 16:40:47 -0400

    4/27 new doamin object

---
+++ src/main/java/com/leanstartup/app/domain/Fee.java
@@ -0,0 +1,30 @@
+package com.leanstartup.app.domain;
+
+import java.util.Date;
+
+public class Fee {
+	private double amount;
+	private Date date;
+	
+	public Fee (double amount, Date date){
+		this.amount = amount;
+		this.date = date;
+				
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	
+	
+}
---
+++ src/main/java/com/leanstartup/app/domain/Vendor.java
@@ -0,0 +1,35 @@
+package com.leanstartup.app.domain;
+
+import java.util.ArrayList;
+
+public class Vendor {
+	
+	private long id;
+    private String name;
+    private ArrayList<Fee> fees;
+    
+    public Vendor (long id, String name, Fee fee){
+    	this.id =id;
+    	this.name =name;
+    	this.fees = new ArrayList<Fee>();
+    	this.fees.add(fee);
+    }
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Fee> getfees() {
+		return fees;
+	}
+	public void setFees(Fee fee) {
+		this.fees.add(fee);
+	}
+}
